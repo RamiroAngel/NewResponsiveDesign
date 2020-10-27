9632930bc463e233c5ccdcabc4a1b29c4d339e80
Ramiro <ramiro.angel@radiumrocket.com>
2020-10-27 01:34:06 -0300

    Excercices: a, b, c from 2)

--- ejercicios-js/javascript-excercises.js
+++ ejercicios-js/javascript-excercises.js
@@ -7,7 +7,7 @@
             var myAge = 27;
             var currentYear = birthYear + myAge;
             
-            console.log(currentYear);
+            console.log (currentYear);
         
         
 /*      B) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.            */
@@ -16,7 +16,7 @@
             var myLastName = 'Angel';
             var myWholeName = myName + myLastName;
             
-            console.log(myWholeName);
+            console.log (myWholeName);
 
 
 /*      C) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
@@ -26,4 +26,34 @@
             var bestGameBoyGame = 'Pokemon Gold';
             var stringCounting = retroNintendo.length + bestGameBoyGame.length;
 
-            console.log(stringCounting);
+            console.log (stringCounting);
+
+
+
+/*  2) Strings
+
+        A) Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula 
+            (utilizar toUpperCase).                                                                                  */
+
+            var song = 'WeControlTheSunlight';
+            var may = song.toUpperCase ();
+            
+            console.log (may);
+
+
+/*      B) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 
+            caracteres guardando el resultado en una nueva variable (utilizar substring).                            */
+
+            var rockStar = 'MikeJagger';
+            var abbv = rockStar.substring (0, 5);
+
+            console.log (abbv);
+
+
+/*      C) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 
+            caracteres guardando el resultado en una nueva variable (utilizar substring).                            */
+
+            var dj = "John 00 Fleming";
+            var lastThree = dj.substring (12);
+
+            console.log (lastThree);
