c6705ea7c59f8c8b43c4bec72a6a6e75e7cd8114
Ramiro <ramiro.angel@radiumrocket.com>
2020-10-27 01:01:25 -0300

    first 3 javascript excercices

---
+++ ejercicios-js/ej-1.js
@@ -0,0 +1,20 @@
+/*  1) Variables y Operadores:
+        
+        A) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er
+        variable.*/
+
+var birthYear = 1993;
+var myAge = 27;
+var currentYear = birthYear + myAge;
+
+console.log(currentYear);
+
+
+/*      B) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.   */
+
+var myName = 'Ramiro'
+var myLastName = 'Angel'
+var myWholeName = myName + myLastName;
+
+console.log(myWholeName);
+
