9d144c8de4e620bddb513d3cd1d5ff713733991b
Ramiro <ramiro.angel@radiumrocket.com>
2020-10-27 18:37:01 -0300

    5 a

--- ejercicios-js/javascript-excercises.js
+++ ejercicios-js/javascript-excercises.js
@@ -204,3 +204,50 @@
                 alert('Anciano');
             
             console.log(age);
+            
+/* 5) For
+            
+        A) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para 
+            mostrar una alerta utilizando cada una de las palabras.                                                  */
+
+            var streets = ['italia', 'cordoba', 'españa', 'dorrego', 'moreno'];
+            var r;
+            for (r = 0; r < streets.length; r++){
+                alert(streets);
+            }
+            
+
+/*      B) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada 
+            palabra modificada.                                                                                      */
+
+
+/*      C) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con 
+            un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única 
+            alerta con la cadena completa.                                                                           */
+
+
+/*      D) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, 
+            es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el 
+            número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final 
+            (utilizar console.log).                                                                                  */
+            
+            
+
+/* 6) Funciones
+            
+/*      A) Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y 
+            guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.  */
+
+/*      B) A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un 
+            número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como 
+            resultado.                                                                                               */
+            
+/*      C) Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número 
+            entero.                                                                                                  */
+            
+/*      D) A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. En caso 
+            que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).  */
+            
+/*      E) Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma 
+            probando que todo siga funcionando igual.                                                                */
+            
